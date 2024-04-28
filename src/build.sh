97d13f2b3c8913575ad6c69396574f5e739cc570
Justin Buhl <justin@kigo.io>
2024-04-28 15:46:57 -0500

    feat: alter build script to work with mac and private flow

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,35 @@
 #!/bin/sh
 
-rm layer/layer.zip
+# Clean up previous layer.zip if it exists
+rm -f layer/layer.zip
 
-docker image build -t openssl-layer .
+# Build the Docker image
+docker build --platform linux/amd64 -t openssl-layer .
 
-docker run --rm -v "$PWD"/layer/:/tmp/layer openssl-layer
+# Run the container in the background and make sure it stays running
+CONTAINER_ID=$(docker run -d openssl-layer tail -f /dev/null)
+
+# Give the container some time to fully initialize
+sleep 10
+
+# Check if layer.zip exists
+if ! docker exec $CONTAINER_ID ls /tmp/layer/layer.zip; then
+    echo "layer.zip does not exist on the container."
+    docker logs $CONTAINER_ID
+    docker stop $CONTAINER_ID
+    exit 1
+fi
+
+# Copy out the layer.zip
+if ! docker cp "${CONTAINER_ID}:/tmp/layer/layer.zip" ./layer/; then
+    echo "Failed to copy layer.zip from container"
+    docker logs $CONTAINER_ID
+    docker stop $CONTAINER_ID
+    exit 1
+fi
+
+# Optionally, print the contents of the lib directory
+docker exec $CONTAINER_ID ls -l /tmp/layer/lib
+
+# Stop the container
+docker stop $CONTAINER_ID
--- src/publish.sh
+++ src/publish.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 LAYER_NAME=openssl-lambda
+REGION=us-east-1
 
-REGIONS="$(aws ssm get-parameters-by-path --path /aws/service/global-infrastructure/services/lambda/regions \
-  --query 'Parameters[].Value' --output text | tr '[:blank:]' '\n' | grep -v -e ^cn- -e ^us-gov- | sort -r)"
+# Publish the Lambda layer in the us-east-1 region
+VERSION_NUMBER=$(aws lambda publish-layer-version --region $REGION --layer-name $LAYER_NAME \
+  --zip-file fileb://layer/layer.zip --cli-read-timeout 0 --cli-connect-timeout 0 \
+  --description "OpenSSL binaries for Amazon Linux 2 Lambdas" --query Version --output text)
 
-for region in $REGIONS; do
-  aws lambda add-layer-version-permission --region $region --layer-name $LAYER_NAME \
-    --statement-id sid1 --action lambda:GetLayerVersion --principal '*' \
-    --version-number $(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME \
-      --zip-file fileb://layer/layer.zip --cli-read-timeout 0 --cli-connect-timeout 0 \
-      --description "OpenSSL binaries for Amazon Linux 2 Lambdas" --query Version --output text)
-done
+# Add a permission to the layer version to keep it private (omit if already private by default)
+aws lambda add-layer-version-permission --region $REGION --layer-name $LAYER_NAME \
+  --statement-id sid1 --action lambda:GetLayerVersion --version-number $VERSION_NUMBER
